080720f71c6ff082a57c0c4d7ff8c7e37b3f2f0f
gorani16 <jgee0116@naver.com>
2022-12-30 02:30:37 +0900

    - navigation 을 사용하여 fragment 간 이동기능 구현. - lifecycle-viewmodel-ktx dependency 추가.
    누락)
    HomeFragment 의 onCreateView 콜백에 return 부분을 inflater 패턴으로 변경

--- app/src/main/java/com/gorani/projectsmilehair/MainActivity.kt
+++ app/src/main/java/com/gorani/projectsmilehair/MainActivity.kt
@@ -2,10 +2,20 @@ package com.gorani.projectsmilehair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
