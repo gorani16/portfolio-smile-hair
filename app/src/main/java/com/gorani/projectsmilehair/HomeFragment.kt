0237d7097cba283b0aaf6df75ff315a4afa2861a
gorani16 <jgee0116@naver.com>
2022-12-30 13:07:16 +0900

    - HomeFragment 에 사용할 JSON 데이터 구조 설계 및 생성 - JSON 데이터를 불러오는 AssetLoader 구현
    누락)
    fragment_home.xml> ImageView, TextView id 추가

--- app/src/main/java/com/gorani/projectsmilehair/HomeFragment.kt
+++ app/src/main/java/com/gorani/projectsmilehair/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.gorani.projectsmilehair
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class HomeFragment: Fragment() {
@@ -14,6 +16,14 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
     }
 
 }
